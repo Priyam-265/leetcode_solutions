136f1279972eee1412e9b650ec135f55872e1b01
Priyam-265 <priyamkathuria265@gmail.com>
2026-09-07 17:33:02 +0530

    Add Medium Java Solution for 940. Fruit Into Baskets from LeetCode

--- LeetCode/Java/Medium/940. Fruit Into Baskets/README.md
+++ LeetCode/Java/Medium/940. Fruit Into Baskets/README.md
@@ -8,8 +8,8 @@
 Array, Hash Table, Sliding Window
 
 ### 🚀 Performance
-- **Runtime:** N/A
-- **Memory:** N/A
+- **Runtime:** 55 ms
+- **Memory:** 70.6 MB
 
 ---
 
--- LeetCode/Patterns/Sliding Window/940. Fruit Into Baskets/Solution.java
+++ LeetCode/Patterns/Sliding Window/940. Fruit Into Baskets/Solution.java
@@ -7,8 +7,8 @@ class Solution {
         for(int r=0;r<n;r++){
             map.put(fruits[r],map.getOrDefault(fruits[r],0)+1);
             while(map.size()>2){
-                map.remove(fruits[l]);
-                map.remove(map.getOrDefault(0,r)-1);
+                map.put(fruits[l],map.getOrDefault(fruits[l],0)-1);
+                if(map.get(fruits[l])==0) map.remove(fruits[l]);
                 l++;
             }
             max=Math.max(max,r-l+1);
